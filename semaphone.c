3dfc55d71d32261e22547ef4aff43654bb86d475
sonalparab <sparab@stuy.edu>
2017-12-09 20:17:56 -0500

    control functions

--- semaphone.c
+++ semaphone.c
@@ -8,65 +8,121 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/shm.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define KEY 831
 
 int main(int argc, char *argv[]){
 
-  int sem;
+  int semid;
+  union semun{
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array; 
+    struct seminfo *__buf;
+  };
+  int shmid;
+  int f;
   int amt = 1;
 
   char* cmd = argv[1];
   //printf("%s\n",cmd);
 
-  int shmd;
-  int f;
 
+  //Creating
   if(!strcmp(cmd,"-c")){
-    if(argv[2]){
-      int val = atoi(argv[2]);
-      shmd = shmget(KEY, sizeof(char*) * val, IPC_CREAT | IPC_EXCL | 0600);
-      sem = semget(KEY,amt,IPC_CREAT | IPC_EXCL | 0600);
-      f = open("story.txt", O_CREAT | O_TRUNC | O_EXCL, 0600);
-      if(sem == -1)
-	       printf("semaphore already exists\n");
-      else{
-      	int v = semctl(sem,0,SETVAL,val);
-      	printf("semaphore created: %d\n",sem);
-      	printf("value set: %d\n",v);
-      }
-    }
-    else
-      printf("Need a value\n");
-  }
-
-  else if(!strcmp(cmd,"-r")){
-    sem = semget(KEY,amt,0600);
-    int v = semctl(sem,0,IPC_RMID);
-    shmd = shmget(KEY, sizeof(char*), 0600);
-    shmdt(&shmd);
-
-    if(v == -1)
-      printf("%s\n",strerror(errno));
+    //create semaphore
+    semid = semget(KEY,amt,IPC_CREAT | IPC_EXCL | 0600);
+    if(semid == -1)
+      printf("could not create semaphore: %s\n",strerror(errno));
     else{
+      union semun su;
+      su.val = 10;
+      int v = semctl(semid,0,SETVAL,su);
+      printf("semaphore created: %d\n",semid);
+      printf("value set: %d\n",v);
+    }
+    
+    //create shared memory
+    shmid = shmget(KEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0600);
+    if(shmid == -1)
+      printf("could not create shared memory: %s\n",strerror(errno));
+    else
+      printf("shared memory created: %d\n",shmid);
+
+    //create story file
+    f = open("story.txt", O_CREAT | O_EXCL | O_TRUNC, 0600);
+    if(f == -1)
+      printf("could not create story file: %s\n",strerror(errno));
+    else
+      printf("story file created: %d\n",f);
+    
+  }
+
+  
+  //Removing
+  else if(!strcmp(cmd,"-r")){
+    semid = semget(KEY,amt,0600);
+    int v = semctl(semid,0,GETVAL);
+    
+    //block if semaphore val is 0
+    while(v == 0)
+      v = semctl(semid,0,GETVAL);
+    
+    //remove semaphore
+    v = semctl(semid,0,IPC_RMID);
+    if(v == -1)
+      printf("could not remove semaphore: %s\n",strerror(errno));
+    else
       printf("semaphore removed: %d \n",v);
-      f = open("story.txt", O_RDONLY | O_TRUNC, 0600);
-      int *buff = malloc(sizeof(char*));
-      read(f, buff, sizeof(buff));
+    
+    //remove shared memory
+    shmid = shmget(KEY, sizeof(int), 0600);
+    char* shm = shmat(shmid,0,0);
+    int s = shmdt(shm);
+    if(s == -1)
+      printf("could not detach shared memory: %s\n",strerror(errno));
+    else
+      printf("memory detached: %d\n", s);
+    int s2 = shmctl(shmid,IPC_RMID,0);
+    if(s2 == -1)
+      printf("could not remove shared memory: %s\n",strerror(errno));
+    else{
+      printf("memory removed: %d\n", s2);
+    }
+
+    //remove story
+    f = open("story.txt", O_RDONLY, 0600);
+    if(f == -1)
+      printf("could not access story: %s\n",strerror(errno));
+    else{
+      struct stat st; 
+      stat("story.txt", &st);
+      char *buffer = malloc(st.st_size);
+      read(f, buffer, sizeof(buffer));
+      printf("story: %s\n",buffer);
+      free(buffer);
+      remove("story.txt");
     }
   }
 
-
+  
+  //Viewing
   else if(!strcmp(cmd,"-v")){
-    sem = semget(KEY,amt,0600);
-    int v = semctl(sem,0,GETVAL);
+    f = open("story.txt", O_RDONLY, 0600);   
 
-    if(v == -1)
-      printf("%s\n",strerror(errno));
-    else
-      printf("semaphore value: %d\n",v);
+    if(f == -1)
+      printf("could not access story: %s\n",strerror(errno));
+    else{
+      struct stat st;
+      stat("story.txt", &st);
+      char *buffer = malloc(st.st_size);
+      read(f, buffer, sizeof(buffer));
+      printf("story: %s\n",buffer);
+      free(buffer);
+    }
   }
-
 
 
   else
